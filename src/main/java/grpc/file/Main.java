44ab977be5d922f705556322b4bd4b6600651d4f
Dwight Thorpe <dthorpe@day2dayja.com>
2025-03-31 08:43:15 -0500

    Update
    Added new service to greeter proto
    Added example to set headers on response

--- src/main/java/grpc/file/Main.java
+++ src/main/java/grpc/file/Main.java
@@ -1,7 +1,15 @@
 package grpc.file;
 
+import grpc.file.examples.GRPCServer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("gRPC Server");
+
+        try{
+            GRPCServer.main(args);
+        } catch (Exception e) {
+            System.err.println("Error starting gRPC server: " + e.getMessage());
+        }
     }
 }
--- src/main/java/grpc/file/examples/GRPCServer.java
+++ src/main/java/grpc/file/examples/GRPCServer.java
@@ -10,15 +10,19 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class GRPCServer {
+
+
     private Server server;
 
     private void start() throws IOException {
         int port = 50051;
-        server = ServerBuilder.forPort(port)
+        server = ServerBuilder
+                .forPort(port)
                 .addService(new GreeterImpl())
+                .addService(new Farewell())
                 .addService(new CalculatorImpl())
+                .intercept(new ServerRequestInterceptor())
                 .addService(ProtoReflectionService.newInstance())
-                .intercept(new MetadataInterceptor())
                 .build()
                 .start();
         System.out.println("Server with reflection started, listening on " + port);
@@ -29,12 +33,24 @@ public class GRPCServer {
             server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
         }
     }
-
     private class GreeterImpl extends GreeterGrpc.GreeterImplBase {
         @Override
         public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
             String resString = "Hello " + req.getName();
-            HelloReply reply = HelloReply.newBuilder()
+            HelloReply reply = HelloReply
+                    .newBuilder()
+                    .setMessage(resString)
+                    .build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+    }
+
+    private class Farewell extends  FarewellGrpc.FarewellImplBase {
+        @Override
+        public void sayGoodbye(ByeRequest req, StreamObserver<ByeReply> responseObserver) {
+            String resString = "Goodbye " + req.getName();
+            ByeReply reply = ByeReply.newBuilder()
                     .setMessage(resString)
                     .build();
             responseObserver.onNext(reply);
@@ -73,42 +89,56 @@ public class GRPCServer {
     public static void main(String[] args) throws IOException, InterruptedException {
         final GRPCServer server = new GRPCServer();
         server.start();
-
-        // Wait for shutdown
         server.server.awaitTermination();
     }
 }
 
-class MetadataInterceptor implements ServerInterceptor {
-
+// Server request Interceptor
+class ServerRequestInterceptor implements io.grpc.ServerInterceptor {
     private static final Metadata.Key<String> AUTH_TOKEN_KEY =
             Metadata.Key.of("auth-token", Metadata.ASCII_STRING_MARSHALLER);
     private static final Metadata.Key<String> USER_ID_KEY =
             Metadata.Key.of("user-id", Metadata.ASCII_STRING_MARSHALLER);
-
+    private static final Metadata.Key<String> SERVER_TOKEN_KEY =
+            Metadata.Key.of("auth-token", Metadata.ASCII_STRING_MARSHALLER);
+    private static final Metadata.Key<String> SERVER_ID =
+            Metadata.Key.of("server-id", Metadata.ASCII_STRING_MARSHALLER);
     @Override
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
             ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
-
         String userId = headers.get(USER_ID_KEY);
         String token = headers.get(AUTH_TOKEN_KEY);
-
-        /*
-
-
-            DO AUTHENTICATION LOGIC HERE
-
-
-         */
-
         if(userId == null || token == null) {
             call.close(Status.UNAUTHENTICATED.withDescription("Missing user ID or auth token"), headers);
             return new ServerCall.Listener<ReqT>() {};
         }
 
+         /*
+
+            DO AUTHENTICATION LOGIC HERE
+
+         */
+
         System.out.println("Received UserId: " + userId);
         System.out.println("Received Token: " + token);
 
-        return next.startCall(call, headers);
+
+        ServerCall<ReqT, RespT> wrappedCall = new ForwardingServerCall.SimpleForwardingServerCall<ReqT, RespT>(call) {
+
+            // Edit message before sending
+            @Override
+            public void sendMessage(RespT message) {
+                System.out.println("sendMessage called: " + message);
+                super.sendMessage(message);
+            }
+            // Set headers before sending
+            @Override
+            public void sendHeaders(Metadata headers) {
+                headers.put(SERVER_TOKEN_KEY, "server_auth_token");
+                headers.put(SERVER_ID, "server_id");
+                super.sendHeaders(headers);
+            }
+        };
+        return next.startCall(wrappedCall, headers);
     }
 }
